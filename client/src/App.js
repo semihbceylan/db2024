421059c78ac186a7ec265a7399477ee9471a76a2
Berkin Zorba <zorbaberkin@gmail.com>
2024-12-03 16:45:04 +0300

    Ädded blocks page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NFTs from './pages/nfts';
 import Chains from './pages/chains';
+import Blocks from './pages/blocks';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/table1" element={<NFTs />} />
         <Route path="/table2" element={<Chains />} />
+        <Route path="/table3" element={<Blocks />} />
       </Routes>
     </Router>
   );

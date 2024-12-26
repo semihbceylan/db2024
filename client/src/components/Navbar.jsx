98821c288ab06054218e46a1824cc21e1a41f973
Berkin Zorba <zorbaberkin@gmail.com>
2024-12-26 15:54:27 +0300

    Bug & typo fixes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png'; 
 import '../styles/navbar.css';
-import nftIcon from '../assets/nft.webp'; // Replace with NFT icon path
-import blockIcon from '../assets/blocks.png'; // Replace with Block icon path
+import nftIcon from '../assets/nft.png'; 
+import blockIcon from '../assets/blocks.png';
 
 const Navbar = () => {
 	return (
@@ -14,16 +14,16 @@ const Navbar = () => {
 					<span className="project-name">DataBees</span>
 				</NavLink>
 				<div className="navbar-links">
-					<NavLink to="/nft" className="nav-link" activeClassName="active-link">
-						<img src={nftIcon} alt="NFT Icon" className="link-icon" />
+					<NavLink to="/nft" className="nav-link" activeclassname="active-link">
+						<img src={nftIcon} alt="NFT Icon" className="nft-icon" />
 						NFT Page
 					</NavLink>
 					<NavLink
 						to="/blocks"
 						className="nav-link"
-						activeClassName="active-link"
+						activeclassname="active-link"
 					>
-						<img src={blockIcon} alt="Block Icon" className="link-icon" />
+						<img src={blockIcon} alt="Block Icon" className="block-icon" />
 						Blocks Page
 					</NavLink>
 				</div>

11c23e800d9ab047fa8e9790d92d437e7f30287a
Berkin Zorba <zorbaberkin@gmail.com>
2024-12-26 15:05:42 +0300

    Added navbar to homepage

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,6 +1,10 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
 export default function Home () {
     return (
         <div>
+            <Navbar />
             <h1>Home</h1>
             <p>Welcome to the home page!</p>
         </div>

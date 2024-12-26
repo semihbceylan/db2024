a4fe7f670e0c319c73d172f015797cc82752c7e7
Berkin Zorba <zorbaberkin@gmail.com>
2024-12-26 15:05:00 +0300

    Added NavBar for webpage

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/logo.png'; 
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<div className="navbar-container">
+				<NavLink to="/" className="logo-container">
+					<img src={logo} alt="Logo" className="logo-image" />
+					<span className="project-name">DataBees</span>
+				</NavLink>
+				<div className="navbar-links">
+					<NavLink to="/nft" className="nav-link" activeClassName="active-link">
+						NFT Page
+					</NavLink>
+					<NavLink
+						to="/blocks"
+						className="nav-link"
+						activeClassName="active-link"
+					>
+						Blocks Page
+					</NavLink>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+
+export default Navbar;
